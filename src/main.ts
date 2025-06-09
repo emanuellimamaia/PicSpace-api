5d7e10f588922f7655ef13760c23f952d1821982
emanuellimamaia <emanuellimamaia@gmail.com>
2025-06-09 14:35:00 -0400

    fix: correction pipe

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(8080);
+  await app.listen(7070);
 
 }
 bootstrap();
